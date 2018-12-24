545413ff9f03296ca43da52a5444adad1eed3a60
Yaz Saito <ysaito@grailbio.com>
2018-12-24 10:00:30 -0800

    added a benchmark

--- README.md
+++ README.md
@@ -3,7 +3,22 @@
 
 Go bindings for cloudflare zlib fork (https://github.com/cloudflare/zlib).
 
-Limitations (as of 2018-12-24):
+## Status
+
+As of 2018-12-24
 
 - only tested for Linux amd64 + cgo.
 - only the reader is implemented.
+
+
+## Benchmark
+
+As of 2018-12-24
+
+It's over 2x faster than the standard implementation.
+
+```
+BenchmarkInflateStandardGzip-8     	     100	  16950147 ns/op
+BenchmarkInflateKlauspostGzip-8    	     100	  20263415 ns/op
+BenchmarkInflateCloudflareGzip-8   	     200	   6976053 ns/op
+```
--- zlib_test.go
+++ zlib_test.go
@@ -3,6 +3,8 @@ package zlib_test
 import (
 	"io"
 	"log"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"math/rand"
@@ -11,12 +13,15 @@ import (
 
 	"bytes"
 
+	"io/ioutil"
+
 	"github.com/grailbio/testutil/assert"
-	"github.com/yasushi-saito/cloudflare-zlib"
+	czlib "github.com/yasushi-saito/cloudflare-zlib"
+	kgzip "github.com/klauspost/compress/gzip"
 )
 
 func testInflate(t *testing.T, r *rand.Rand, src []byte, want []byte) {
-	zin, err := zlib.NewReader(bytes.NewReader(src))
+	zin, err := czlib.NewReader(bytes.NewReader(src))
 	assert.NoError(t, err)
 
 	var (
@@ -59,4 +64,57 @@ func TestInflateRandom(t *testing.T) {
 		assert.NoError(t, gz.Close())
 		testInflate(t, r, compressed.Bytes(), uncompressed)
 	}
+}
+
+func benchmarkInflate(
+	b *testing.B,
+	path string,
+	inflateFactory func(in io.Reader) (io.Reader, error)) {
+	b.StopTimer()
+	tmp, err := ioutil.TempDir("", "")
+	assert.NoError(b, err)
+	defer os.RemoveAll(tmp)
+
+	in, err := os.Open(path)
+	assert.NoError(b, err)
+	dstPath := filepath.Join(tmp, "tmp.gz")
+	out, err := os.Create(dstPath)
+	assert.NoError(b, err)
+	outgz := gzip.NewWriter(out)
+	wantByte, err := io.Copy(outgz, in)
+	assert.NoError(b, err)
+	assert.NoError(b, outgz.Close())
+	assert.NoError(b, in.Close())
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		in, err = os.Open(dstPath)
+		assert.NoError(b, err)
+		inflator, err := inflateFactory(in)
+		assert.NoError(b, err)
+		n, err := io.Copy(ioutil.Discard, inflator)
+		assert.NoError(b, err)
+		assert.EQ(b, n, wantByte)
+	}
+}
+
+func BenchmarkInflateStandardGzip(b *testing.B) {
+	benchmarkInflate(b, "/tmp/get-pip.py",
+		func(in io.Reader) (io.Reader, error) {
+			return gzip.NewReader(in)
+		})
+}
+
+func BenchmarkInflateKlauspostGzip(b *testing.B) {
+	benchmarkInflate(b, "/tmp/get-pip.py",
+		func(in io.Reader) (io.Reader,error) {
+			return kgzip.NewReader(in)
+		})
+}
+
+func BenchmarkInflateCloudflareGzip(b *testing.B) {
+	benchmarkInflate(b, "/tmp/get-pip.py",
+		func(in io.Reader) (io.Reader,error) {
+			return czlib.NewReader(in)
+		})
 }
